3b1271eee4154104acf5ce70711995d54faa7e46
Leon <leon@wp2static.com>
2019-05-04 02:00:41 +0700

    describe offline conversion function

--- plugin/WP2Static/HTMLProcessingFunctions/convertToOfflineURL.php
+++ plugin/WP2Static/HTMLProcessingFunctions/convertToOfflineURL.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+    When publishing our static site for offline usage, we need to ensure a few
+    transformations are made:
+
+     - with no site relativity possible to rely on, we need all links to be
+       document relative, ie to go to parent page, use ../page not /page or
+       file:///page (allowing for C:\page,/home/me/page, etc
+
+     - with no webserver to render default documents like /index.html, we need
+       to rewrite all links to full post/index.html style URLs
+
+*/
 function convertToOfflineURL( $url_to_change, $page_url, $placeholder_url  ) {
     $current_page_path_to_root = '';
     $current_page_path = parse_url( $page_url, PHP_URL_PATH );
